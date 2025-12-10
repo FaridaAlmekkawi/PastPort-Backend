5d5f530af36f56a33c33107ac92d4756e10e4f30
BLACKBOX Agent <code@blackbox.ai>
2025-12-10 17:06:44 +0000

    IInvoiceRepository.cs ISavedPaymentMethodReposito...

---
+++ PastPort.Domain/Interfaces/IAppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PastPort.Domain.Interfaces;
+
+public interface IAppSettings
+{
+    // Define app settings properties here
+    string DatabaseConnectionString { get; }
+    string StripeSecretKey { get; }
+    string PayPalClientId { get; }
+    string PayPalClientSecret { get; }
+    // Add other settings as needed
+}
